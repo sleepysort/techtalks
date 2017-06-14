8882b1434b1e1d3b0642da8b47d2352188f801ec
Shawn Lee <shawnsmlee@gmail.com>
2017-06-14 09:54:03 -0700

    Added all tests

---
+++ webdev/talk1/exercises/exercises.js
@@ -0,0 +1,60 @@
+/**
+ * Exercise 1:
+ * 
+ * Write a function that computes powers/exponentiation, using the given numbers. 
+ * Do not use Math.pow().
+ * You may assume that 'exp' will be non-negative.
+ * 
+ * @param base  The base number.
+ * @param exp   The exponent number.
+ * @returns  The computed value of 'base' to the 'exp' power.
+ */
+function pow(base, exp) {
+    /** Add code here */
+}
+module.exports.pow = pow;
+
+/**
+ * Exercise 2:
+ * 
+ * Write a function that creates a 2D-vector. The vector should be an object with 'x' and 'y' properties.
+ * 
+ * @param x  The number to use as the x value of the vector
+ * @param y  The number to use as the y value of the vector
+ * @returns A vector with the given x and y.
+ */
+function create2dVector(x, y) {
+    /** Add code here */
+}
+module.exports.create2dVector = create2dVector;
+
+
+/**
+ * Exercise 3:
+ * 
+ * Write a function that takes two parameters (a number and a function), and calls the function with two
+ * times that number. 
+ * 
+ * @param n  The number to use.
+ * @param cb The callback function to use.
+ */
+function callWithDouble(n, cb) {
+    /** Add code here */
+}
+module.exports.callWithDouble = callWithDouble;
+
+
+/**
+ * Exercise 4:
+ * 
+ * Write a function that composes two functions together, and returns the composed function.
+ * On function composition: http://www.purplemath.com/modules/fcncomp3.htm
+ * 
+ * @param f  The first function to compose.
+ * @param g  The second function to compose.
+ * @returns The new function that is the compose of f and g.
+ */
+function compose(f, g) {
+
+}
+module.exports.compose = compose;
